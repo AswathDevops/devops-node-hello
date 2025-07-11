cffd712e8299cee9831f731c0e7aa49f3fa8ac0b
AswathDevops <aswaththamman.s@gmail.com>
2025-07-11 16:46:45 +0530

    update indx

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Theju How are  you!\n'
+  const msg = 'Hello how are  you!\n'
   res.end(msg);
 });
 
