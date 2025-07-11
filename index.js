fe10133f906b6918ec1e661c129ecf0c3161257b
AswathDevops <aswaththamman.s@gmail.com>
2025-07-11 13:16:15 +0530

    updated in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Ajeesh Nitish Rajesh this is my mini project!\n'
+  const msg = 'Hello Ajeesh Nitish Rajesh , Coimbatore la meet panniduvoma? on July 19?!\n'
   res.end(msg);
 });
 
